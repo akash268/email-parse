3fceb016f0292851e72e89b032bffa1a1ec92207
Akash_Malik <akashmalik268@gmail.com>
2016-07-04 18:20:19 +0530

    now removes duplicates

--- app/app.component.ts
+++ app/app.component.ts
@@ -18,21 +18,37 @@ declare var Clipboard: any;
                <div class="cell" id="process-block">
                  <button id="copy" class="btn" data-clipboard-target="#output">copy</button>
                </div>
+              </div>
+              <div *ngIf="duplicateCount > 0" class="error">
+                <!-- Error message displayed when the errorCount property on the current context is greater
+                      than 0. -->
+                {{duplicateCount}} duplicates removed
               </div> 
              `
 })
-export class AppComponent { 
+export class AppComponent {
+  
+  //no. of duplicate entries
+  duplicateCount: number = 0; 
+
   process(input, output){
     console.log(input);
     //variable for holding each email
     var email;
+    //emailArray
+    var emailArray=[];    
     for(var i=0;i<input.length;i++){
       var ch = input[i];
-      if( ch == '<'){
+      if( ch == '<' || ch == '('){
         email="";
       }else if(ch == '@'){
         console.log(email);
-        output.value += email + " \n";
+        if(emailArray.indexOf(email) == -1){
+          emailArray.push(email);
+          output.value += email + " \n";
+        }else{
+          this.duplicateCount++;
+        }        
       }else{
         email+=ch;
       }
